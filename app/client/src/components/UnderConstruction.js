bd4eea5c7ad42d33cb71bc0a8c188cbde6e6d567
Jeremiah <jeremiahss@hotmail.com>
2022-05-27 16:17:37 -0700

    MVP, awaiting input

---
+++ app/client/src/components/UnderConstruction.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card, Container } from "react-bootstrap";
+import { CompanyJumbotron } from "./CompanyJumbotron/JumboIndex";
+
+export const UnderConstruction = () => {
+  return (
+    <div>
+      <CompanyJumbotron header={"Under Construction"} />
+      <Container className="d-flex flex-column min-vh-100 pb-50">
+        <Card className="p-5" bg="dark" text="white">
+          <Card.Title>This page is under construction</Card.Title>
+          <Card.Text>Check back soon for more updates!</Card.Text>
+        </Card>
+      </Container>
+    </div>
+  );
+};
