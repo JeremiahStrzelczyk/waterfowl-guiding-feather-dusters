d360cadb2db56c71842c988a16905242d62b20fe
JeremiahStrzelczyk <Jeremiah.Strzelczyk@WSU.edu>
2023-02-02 12:14:17 -0800

    Ready for test deploy

--- app/client/src/components/CallToAction.js
+++ app/client/src/components/CallToAction.js
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const CallToAction = () => {
+  const navigate = useNavigate();
   return (
     <div className="cta-wrapper">
       <div className="cta">
         <p className="center">We're bringing in the geese this year.</p>
         <p className="center">You can to.</p>
-        <button className="button">
-          <Link to="/contact" className="cta__link">
-            Book Today!
-          </Link>
+        <button
+          className="button cta__link"
+          onClick={() => navigate("/contact")}
+        >
+          Book Today!
         </button>
       </div>
     </div>
